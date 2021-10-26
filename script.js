eda6c0eedf8a7edc2bc92b66e569c164b012e989
JairNeto1 <jairmonteironeto@gmail.com>
2021-10-26 08:27:14 -0300

    upadating htlm and CSS

--- script.js
+++ script.js
@@ -10,8 +10,23 @@ function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
-    console.log(li.children[0].href)
+    // console.log(li.children[0].href)
   }
 }
 
+function getGithubProfileInfo() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      // userLink.href = data.html_url
+      // userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
 changeSocialMediaLinks()
+getGithubProfileInfo()
